5a0a2e36f46487cbffb15d8a81eccec5c1050c08
thach1206 <thachmonster12062003@gmail.com>
2023-09-07 09:52:50 +0700

    test

---
+++ main.js
@@ -0,0 +1,41 @@
+/*var fullName = 'Doan Van Thach';
+var age = 26;
+alert(fullName)
+alert(age)
+
+var fullName = 'Doan Van Thach'
+console.log(fullName)
+
+confirm('xac nhan ban du tuoi ')
+
+prompt('xac nhan ban du tuoi ')
+
+setTimeout(function() {
+    alert('ban ch du tuoi')
+},1000)
+
+setInterval(function() {
+    console.log('ban ch du tuoi'+ Math.random())
+},1000)
+*/
+var a = 1;
+var b = 2;
+/*if (a == b){
+    console.log('dung')
+}
+else
+alert('sai')
+*/
+var c = a %  b;
+//console.log(c);
+++a
+//console.log(a);
+var firstName = 'Thach';
+//var lastName ='Doan';
+firstName += ' Doan'
+//console.log(firstName+' '+lastName)
+console.log(firstName);
+//<h1>hi javascript basic</h1>
+ //   <script src="./main.js"></script>
+
+
